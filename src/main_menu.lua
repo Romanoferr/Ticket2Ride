32a1f5a06a4f6e5473215293217d871ef457d2e5
EMV2.71828 <emv2.71828@gmail.com>
2025-05-26 19:29:51 -0300

    retirada do game manager, adicao dos botoes para a funcao draw

--- src/main_menu.lua
+++ src/main_menu.lua
@@ -1,4 +1,3 @@
-local gameManager = require "gameManager"
 local lovebird = require "libs.lovebird"
 
 function create_button(text, fn)
@@ -38,15 +37,30 @@ function love.load()
         table.insert(buttons, create_button(v[1], v[2]))
     end
 
-    gameManager.draw()
 
 end
 
 function love.update(dt)
     lovebird.update()
-    gameManager.update(dt)
 end
 
 function love.draw()
-    gameManager.draw()
+    local ww = love.grahics.getWidth()
+    local wh = love.grahics.getHeight()
+
+    local button_width = ww * (1 / 3)
+    local button_height = 64
+
+    local margin = 16
+
+    for i, button in pairs(buttons) do
+        love.graphics.rectangle(
+                "fill",
+                (ww * 0.5) - (button_width * 0.5),
+                (wh * 0.5) - (button_height * 0.5),
+                button_width,
+                button_height
+        )
+    end
+
 end
