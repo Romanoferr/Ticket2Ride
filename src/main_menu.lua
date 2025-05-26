1e401518f2086964273c2a7cbfa7d7022e6702a6
EMV2.71828 <emv2.71828@gmail.com>
2025-05-26 18:52:15 -0300

    criacao de valores dos botoes

--- src/main_menu.lua
+++ src/main_menu.lua
@@ -1,7 +1,33 @@
+function create_button(text, fn)
+    return {
+        text = text,
+        fn = fn
+    }
+end
+
 local buttons = {}
+
+local button_values = {
+    { "Iniciar Jogo", function()
+        print("Iniciando jogo")
+    end },
+    { "Opções", function()
+        print("iniciando opções")
+    end },
+    { "Sair", function()
+        love.event.quit()
+    end }
+}
 
 function love.load()
 
+    for _, v in pairs(button_values) do
+
+        table.insert(buttons, createButton(
+                v[]
+        )
+        )
+    end
 end
 
 function love.update(dt)
