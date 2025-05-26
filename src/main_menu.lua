c54ed0ac6c18028c54881a9e6674e92e5e1ebeba
EMV2.71828 <emv2.71828@gmail.com>
2025-05-26 19:04:35 -0300

    adicao das dimensoes da janela e do gameManager

--- src/main_menu.lua
+++ src/main_menu.lua
@@ -1,3 +1,5 @@
+local gameManager = require "gameManager"
+
 function create_button(text, fn)
     return {
         text = text,
@@ -5,35 +7,45 @@ function create_button(text, fn)
     }
 end
 
-local buttons = {}
+local settings = {
+    fullscreen = false,
+    screenScaler = 1,
+    logicalWidth = 1280,
+    logicalHeight = 720
+}
 
 local button_values = {
-    { "Iniciar Jogo", function()
-        print("Iniciando jogo")
-    end },
-    { "Opções", function()
-        print("iniciando opções")
-    end },
-    { "Sair", function()
-        love.event.quit()
-    end }
+    { "Iniciar Jogo",
+      function()
+          print("Iniciando jogo")
+      end },
+    { "Opções",
+      function()
+          print("Iniciando opções")
+      end },
+    { "Sair",
+      function()
+          love.event.quit(0)
+      end }
 }
+
+local buttons = {}
 
 function love.load()
 
     for _, v in pairs(button_values) do
-
-        table.insert(buttons, createButton(
-                v[]
-        )
-        )
+        table.insert(buttons, create_button(v[1], v[2]))
     end
+
+    gameManager.draw()
+
 end
 
 function love.update(dt)
 
+
 end
 
 function love.draw()
-
+    gameManager.draw()
 end
