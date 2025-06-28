cf3f60a4e14c94c088267115503e8a6db2a55d25
EMV2.71828 <emv2.71828@gmail.com>
2025-06-28 17:36:45 -0300

    adicao de botoes

--- src/opcoes.lua
+++ src/opcoes.lua
@@ -1,21 +1,57 @@
 local gameManager
 
 local opcoes = {
+    buttons = {}
+}
 
+function create_buttons(image, fn)
+    return {
+        image = love.graphics.newImage(image),
+        fn = fn
+    }
+end
+
+button_values = {
+    {
+        "assets/opcoes_menu_checkbutton.png",
+        function()
+            gameManager.changeState("mainMenu")
+        end
+    },
+    {
+        "assets/opcoes_menu_exitbutton.png",
+        function()
+            gameManager.changeState("mainMenu")
+        end
+    }
 }
 
 function opcoes.load()
     gameManager = require "gameManager"
+
     background = love.graphics.newImage("assets/opcoes_menu.png")
+
+    for _, row in ipairs(button_values) do
+        table.insert(opcoes.buttons, create_buttons(row[1], row[2]))
+    end
+
 end
 
 function opcoes.draw()
     local windowWidth, windowHeight = love.graphics.getDimensions()
+
     local imageWidth, imageHeight = background:getWidth(), background:getHeight()
+
 
     love.graphics.setBackgroundColor(0.6, 0.2, 0.1)
 
-    love.graphics.draw(background, windowWidth/imageWidth + 200, windowHeight/imageHeight , 0, 0.5, 0.4)
+    love.graphics.draw(background, windowWidth / imageWidth + 250, windowHeight / imageHeight, 0, 0.5, 0.4)
+
+    love.graphics.draw(opcoes.buttons[1].image, 400, 500, 0, 0.4, 0.4)
+
+    love.graphics.draw(opcoes.buttons[2].image, 750, 500, 0, 0.4, 0.4)
+
+
 end
 
 return opcoes
