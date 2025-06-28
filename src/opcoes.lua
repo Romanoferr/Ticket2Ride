4bdd03238bb59d37205de4b0090eddc0816f775b
EMV2.71828 <emv2.71828@gmail.com>
2025-06-28 16:46:05 -0300

    adicao de parte do menu

--- src/opcoes.lua
+++ src/opcoes.lua
@@ -6,10 +6,16 @@ local opcoes = {
 
 function opcoes.load()
     gameManager = require "gameManager"
+    background = love.graphics.newImage("assets/opcoes_menu.png")
 end
 
 function opcoes.draw()
+    local windowWidth, windowHeight = love.graphics.getDimensions()
+    local imageWidth, imageHeight = background:getWidth(), background:getHeight()
+
     love.graphics.setBackgroundColor(0.6, 0.2, 0.1)
+
+    love.graphics.draw(background, windowWidth/imageWidth + 200, windowHeight/imageHeight , 0, 0.5, 0.4)
 end
 
 return opcoes
