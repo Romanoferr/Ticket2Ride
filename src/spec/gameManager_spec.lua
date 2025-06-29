947cde39663df1c62483a9aea15c5646692a80b1
EMV2.71828 <emv2.71828@gmail.com>
2025-06-29 10:29:46 -0300

    criacao de testes unitarios usando busted

---
+++ src/spec/gameManager_spec.lua
@@ -0,0 +1,14 @@
+local gameManager = require "gameManager"
+
+describe('gameManager state', function()
+    it('deve iniciar no estado mainMenu', function()
+        assert.are.equal(gameManager.state, 'mainMenu')
+    end)
+
+    it('deve ser capaz de alterar entre estados que existam no jogo', function()
+        gameManager.changeState('opcoes')
+        assert.are.equal(gameManager.state, 'opcoes')
+    end)
+
+end)
+
