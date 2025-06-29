6094c053123db57436c4bfa933a0ffe9bba56a20
EMV2.71828 <emv2.71828@gmail.com>
2025-06-29 10:43:48 -0300

    adicao de mais um teste

--- src/spec/gameManager_spec.lua
+++ src/spec/gameManager_spec.lua
@@ -5,6 +5,10 @@ describe('gameManager state', function()
         assert.are.equal(gameManager.state, 'mainMenu')
     end)
 
+    it('o score inicial deve ser zero', function()
+        assert.are.equal(gameManager.score, 0)
+    end)
+
     it('deve ser capaz de alterar entre estados que existam no jogo', function()
         gameManager.changeState('opcoes')
         assert.are.equal(gameManager.state, 'opcoes')
